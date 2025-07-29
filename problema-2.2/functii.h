a45b45ea569cab40ed1ef322d0637238fbf2a34e
Serban Amariei-Hondrea <amarieiserban2@gmail.com>
2025-07-29 19:15:18 +0300

    Create functii.h

---
+++ problema-2.2/functii.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTII_H_
+#define FUNCTII_H_
+
+void citire(int a[][10], int n, int m);
+void transpusa(int a[][10], int at[][10], int n, int m);
+void produs(int p[][10], int a[][10], int b[][10], int n, int m);
+void afisare(int a[][10], int n, int m);
+
+#endif
